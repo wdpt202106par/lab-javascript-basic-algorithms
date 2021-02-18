45bf8e75f86dbbabfc52e5decc413fd7e9cbbd16
Tran Van <vanbaotran201194@gmail.com>
2021-02-18 21:03:22 +0100

    iteration 1 done

--- js/index.js
+++ js/index.js
@@ -1,5 +1,8 @@
 // Iteration 1: Names and Input
-
+let hacker1 = "Tran";
+console.log (`The driver's name is ${hacker1}`);
+let hacker2 = "Alexandre";
+console.log (`The navigator's name is ${hacker2}`);
 
 // Iteration 2: Conditionals
 
